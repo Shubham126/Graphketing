1de9b66d8ec896a7af6b506eb5b6a7f020a44eca
Subham <chromaticshubham26@gmail.com>
2025-06-20 16:10:52 +0530

    small changes in server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 10000;
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
-app.use('/api/users', require('./routes/users'));
+// Add your routes here
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
